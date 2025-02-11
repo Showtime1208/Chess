ecdbf028d96357f78317ead72159783d30cc8693
Showtime1208 <andresmattonconover@gmail.com>
2025-02-11 00:56:43 -0500

    Implemented Queen logic, finishing all pieces.

--- src/model/piece/Queen.java
+++ src/model/piece/Queen.java
@@ -2,6 +2,7 @@ package model.piece;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 import model.board.Board;
 
 import java.awt.*;
@@ -20,23 +21,7 @@ public class Queen implements ChessPiece {
 
   @Override
   public List<Point> getValidMoves(Board boardState) {
-    List<Point> validMoves = new ArrayList<>();
-    for (int nRow = -1; nRow <= 1; nRow++) {
-      for (int nCol = -1; nCol <= 1; nCol++) {
-        if (nRow == 0 && nCol == 0) {
-          continue;
-        }
-        int newRow = row + nRow;
-        int newCol = col + nCol;
-        if (isInBounds(newRow, newCol)) {
-          ChessPiece squareOccupant = boardState.get(newRow, newCol);
-          if (squareOccupant == null || squareOccupant.isWhite() != this.isWhite) {
-            validMoves.add(new Point(row, col));
-          }
-        }
-      }
-    }
-    return validMoves;
+    return Stream.concat(getValidBishopMoves(boardState).stream(), getValidRookMoves(boardState).stream()).toList();
   }
 
   private boolean isInBounds(int row, int col) {
@@ -58,5 +43,127 @@ public class Queen implements ChessPiece {
   @Override
   public int getPointValue() {
     return 9;
+  }
+
+  private List<Point> getValidBishopMoves(Board boardState) {
+      List<Point> validMoves = new ArrayList<>();
+      //down and right
+      for (int newRow = row + 1; newRow < 8; newRow++) {
+          for (int newCol = col + 1; newCol < 8; newCol++) {
+              if (!isInBounds(newRow, newCol)) break;
+              ChessPiece occupant = boardState.get(newRow, newCol);
+              if (occupant == null) {
+                  validMoves.add(new Point(newRow, newCol));
+              } else {
+                  if (occupant.isWhite() != this.isWhite) {
+                      validMoves.add(new Point(newRow, newCol));
+                  }
+                  break;
+              }
+          }
+      }
+      //up and right
+      for (int newRow = row - 1; newRow >= 0; newRow--) {
+          for (int newCol = col + 1; newCol < 9; newCol++) {
+              if (!isInBounds(newRow, newCol)) break;
+              ChessPiece occupant = boardState.get(newRow, newCol);
+              if (occupant == null) {
+                  validMoves.add(new Point(newRow, newCol));
+              } else {
+                  if (occupant.isWhite() != this.isWhite) {
+                      validMoves.add(new Point(newRow, newCol));
+                  }
+                  break;
+              }
+          }
+      }
+      //down and left
+      for (int newRow = row + 1; newRow < 8; newRow++) {
+          for (int newCol = col - 1; newCol >= 0; newCol--) {
+              if (!isInBounds(newRow, newCol)) break;
+              ChessPiece occupant = boardState.get(newRow, newCol);
+              if (occupant == null) {
+                  validMoves.add(new Point(newRow, newCol));
+              } else {
+                  if (occupant.isWhite() != this.isWhite) {
+                      validMoves.add(new Point(newRow, newCol));
+                  }
+                  break;
+              }
+          }
+      }
+      //down and right
+      for (int newRow = row - 1; newRow >= 0; newRow--) {
+          for (int newCol = col - 1; newCol >= 0; newCol--) {
+              if (!isInBounds(newRow, newCol)) break;
+              ChessPiece occupant = boardState.get(newRow, newCol);
+              if (occupant == null) {
+                  validMoves.add(new Point(newRow, newCol));
+              } else {
+                  if (occupant.isWhite() != this.isWhite) {
+                      validMoves.add(new Point(newRow, newCol));
+                  }
+                  break;
+              }
+          }
+      }
+      return validMoves;
+  }
+
+  private List<Point> getValidRookMoves(Board boardState) {
+      List<Point> validMoves = new ArrayList<>();
+      //Going up
+      for (int newRow = row - 1; newRow >= 0 ; newRow--) {
+          if (!isInBounds(newRow, col)) break;
+          ChessPiece occupant = boardState.get(newRow, col);
+          if (occupant == null) {
+              validMoves.add(new Point(newRow, col));
+          } else {
+              if (occupant.isWhite() != this.isWhite) {
+                  validMoves.add(new Point(newRow, col));
+              }
+              break;
+          }
+      }
+      //Going down
+      for (int newRow = row + 1; newRow < 8 ; newRow++) {
+          if (!isInBounds(newRow, col)) break;
+          ChessPiece occupant = boardState.get(newRow, col);
+          if (occupant == null) {
+              validMoves.add(new Point(newRow, col));
+          } else {
+              if (occupant.isWhite() != this.isWhite) {
+                  validMoves.add(new Point(newRow, col));
+              }
+              break;
+          }
+      }
+      //going left
+      for (int newCol = col - 1; newCol >= 0 ; newCol--) {
+          if (!isInBounds(row, newCol)) break;
+          ChessPiece occupant = boardState.get(row, newCol);
+          if (occupant == null) {
+              validMoves.add(new Point(row, newCol));
+          } else {
+              if (occupant.isWhite() != this.isWhite) {
+                  validMoves.add(new Point(row, newCol));
+              }
+              break;
+          }
+      }
+      //going right
+      for (int newCol = col + 1; newCol < 8 ; newCol++) {
+          if (!isInBounds(row, newCol)) break;
+          ChessPiece occupant = boardState.get(row, newCol);
+          if (occupant == null) {
+              validMoves.add(new Point(row, newCol));
+          } else {
+              if (occupant.isWhite() != this.isWhite) {
+                  validMoves.add(new Point(row, newCol));
+              }
+              break;
+          }
+      }
+      return validMoves;
   }
 }
